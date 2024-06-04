98584d2b770560878cf87480ce7b74f8cd26b49d
ggleroy <112895265+ggleroy@users.noreply.github.com>
2024-06-03 21:42:13 -0300

    Add NoticeStatus model

---
+++ Codigo/Cycling-api/src/main/java/com/CyclingConnect/cyclingconnect/models/notices/NoticeStatus.java
@@ -0,0 +1,28 @@
+package com.CyclingConnect.cyclingconnect.models.notices;
+
+/**
+ * A enumeração NoticeStatus representa os diferentes status de um aviso.
+ *
+ * <p>Os status possíveis são:
+ * <ul>
+ *     <li><strong>ACTIVE</strong>: Indica que o aviso está ativo e visível no quadro de avisos.</li>
+ *     <li><strong>INACTIVE</strong>: Indica que o aviso não está mais ativo ou visível no quadro de avisos.</li>
+ * </ul>
+ *
+ * @param status O status do aviso.
+ */
+public enum NoticeStatus {
+    ACTIVE("ACTIVE"),
+    INACTIVE("INACTIVE");
+
+    private final String status;
+
+    NoticeStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
+ß
